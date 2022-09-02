4c1cc3de16efa514549a6f0a50bfb5d087be3332
eyp <eypsay@yahoo.com>
2022-09-02 21:47:44 +0300

    Add project files.

---
+++ src/kodlamaIoDevs/Application/Services/Repositories/IProgrammingLanguageRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+
+namespace Application.Services.Repositories
+{
+    public interface IProgrammingLanguageRepository : IAsyncRepository<ProgrammingLanguage>, IRepository<ProgrammingLanguage>
+    {
+        // ProgrammingLanguage özel operasyonlar olursa buraya yazılır
+        // 
+
+    }
+}
